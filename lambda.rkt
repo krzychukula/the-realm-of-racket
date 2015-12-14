5795879ffb0f00a56bc35c9cac964544d8f4ad0b
krzychukula <krzychukula@gmail.com>
2015-12-14 07:37:36 +0100

    or map and filter

--- lambda.rkt
+++ lambda.rkt
@@ -14,3 +14,18 @@
 ;;          goos))
 
 ((lambda (num) (- num 2)) 5)
+
+(define (my-filter pred lst)
+  (cond
+    [(empty? lst) empty]
+    [(pred (first lst))
+     (cond (first lst)
+           (my-filter pred (rest lst)))]
+    [else (my-filter pred (rest lst))]))
+
+(define (my-ormap pred lst)
+  (cond
+    [(empty? lst) #f]
+    [else (or (pred (first lst))
+              (my-ormap pred (rest lst)))]))
+
