298e7df7ff9cc2decc9dff961052c6cd95fee3dc
krzychukula <krzychukula@gmail.com>
2015-12-18 18:07:39 +0100

    some dummy text but this is not going to work as well - giving up

--- dice-of-doom.rkt
+++ dice-of-doom.rkt
@@ -20,3 +20,21 @@
 
 (define mv0 (move '(0 1) gt1))
 (define gt0 (game b0 0 (list mv0)))
+
+
+(define (create-world-of-dice-and-doom)
+  (define board (territory-build))
+  (define gamet (game-tree board INIT-PLAYER INIT-SPARE-DICE))
+  (define new-world (dice-world #f board gamet))
+  (if (no-more-moves-in-world? new-world)
+      (create-world-of-dice-and-doom)
+      new-world))
+
+
+
+(define (roll-the-dice)
+  (big-bang (create-world-of-dice-and-doom)
+            (on-key interact-with-board)
+            (on-draw draw-dice-world)
+            (stop-when no-more-moves-in-world?
+                       draw-end-of-dice-world)))
